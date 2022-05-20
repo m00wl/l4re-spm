1f78509bcc93266a80cfcae61fd05a28c6a201e2
Moritz Lumme <46034439+m00wl@users.noreply.github.com>
2022-05-20 11:56:22 +0200

    add nix flake for l4re-spm

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "l4re-spm flake";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    pkgsArm64 = pkgs.pkgsCross.aarch64-multiplatform;
+  in
+  {
+    #devShell = pkgsArm64.mkShell.override { stdenv = pkgsArm64.gcc10Stdenv; } {
+    devShell = pkgsArm64.mkShell {
+      CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
+      nativeBuildInputs = [
+        pkgs.gcc
+        pkgs.dialog
+        pkgs.gawk
+        pkgs.binutils
+        pkgs.pkg-config
+        pkgs.flex
+        pkgs.bison
+        pkgs.dtc
+        pkgs.ubootTools
+      ];
+    };
+  }
+  );
+
+  nixConfig = {
+    bash-prompt-suffix = "dev: ";
+  };
+}
